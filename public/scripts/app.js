4da4e676f646720d4095d4231361925890692c74
Kelly Garzon <kelly.garzon@viamericas.com>
2018-11-15 17:02:28 -0500

    Cambio 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,7 +13,7 @@ console.log('App.js is running!');
 var template = React.createElement(
     'h1',
     null,
-    'Indecision App'
+    'Cambio 2 App'
 );
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
