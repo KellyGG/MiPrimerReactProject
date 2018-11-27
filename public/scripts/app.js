e2ea6e61e74e9479200b20c245f29a33cace8592
Kelly Garzon <kelly.garzon@viamericas.com>
2018-11-27 09:21:11 -0500

    parte 2 del curso de react

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,10 +10,66 @@ console.log('App.js is running!');
     { id: "someid" },
     "Something new"
   );*/
+//JSX - Javascript XML
 var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
     'h1',
     null,
-    'Cambio 2 App'
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item three'
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Kelly G'
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' Age: 25'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location:Medellin'
+  )
+);
+//Create a templateTwo var JSX expression
+//div
+//h1 -> Kelly G
+//p-> Age: 25
+//p->Location:Medellin
+//Render templateTwo instead of template
+
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -8,6 +8,35 @@ console.log('App.js is running!');
     { id: "someid" },
     "Something new"
   );*/
-var template= <h1>Indecision App</h1>;
+  //JSX - Javascript XML
+var template=(
+<div>
+  <h1>Indecision App</h1>
+  <p> This is some info</p>
+  <ol>
+    <li>Item one</li>
+    <li>Item two</li>
+    <li>Item three</li>
+
+  </ol>
+</div>
+);
+
+var templateTwo=
+(
+  <div>
+    <h1>Kelly G</h1>
+    <p> Age: 25</p>
+    <p>Location:Medellin</p>
+  
+  </div>
+  )
+//Create a templateTwo var JSX expression
+//div
+//h1 -> Kelly G
+//p-> Age: 25
+//p->Location:Medellin
+//Render templateTwo instead of template
+
 var appRoot=document.getElementById('app');
-ReactDOM.render(template,appRoot);
+ReactDOM.render(templateTwo,appRoot);
